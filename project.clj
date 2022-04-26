63c23e551e5869bbcda55a5d32af1e9b3eea165b
Funkschy <felix.schoeller@protonmail.com>
2022-04-26 18:34:51 +0200

    removed reflection warning and snapshot

--- project.clj
+++ project.clj
@@ -1,6 +1,5 @@
-(defproject tinysegmenter "0.1.0-SNAPSHOT"
+(defproject tinysegmenter "0.1.0"
   :description "A Clojure library to split Japanese into words"
   :license {:name "BSD 3-Clause 'New' or 'Revised' License"}
   :dependencies [[org.clojure/clojure "1.10.3"]]
-  :global-vars {*warn-on-reflection* true}
   :repl-options {:init-ns tinysegmenter.core})
